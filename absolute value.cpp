13d017ee0e01b48a60c167972b5185b030c7bfaa
devashishkumar7 <btech60047.22@bitmesra.ac.in>
2024-10-03 11:31:26 +0530

    Create absolute value.cpp

---
+++ absolute value.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<" enter the number"<<endl;
+    if (n>0)
+    {
+        cout<<"absolut value :"<<n<<endl;
+
+    }
+    else if (n<0)
+    {
+        cout << "value :"<<-n<<endl;
+    }
+
+}
