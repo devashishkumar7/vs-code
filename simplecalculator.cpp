4fed4e6e3448311666f6f2fe69ad38837da76e76
devashishkumar7 <btech60047.22@bitmesra.ac.in>
2024-10-06 13:12:26 +0530

    Add files via upload

---
+++ simplecalculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+     char c;
+    cin>>c;
+    int b;
+    cin>>b;
+    if(c=='+'){
+        cout<<a+b<<endl;
+    }
+    else if(c =='-'){
+        cout<<a-b<<endl;
+    }
+    else  if ( c == '/'){
+        cout<<a/b<<endl;
+    }
+    else if (c =='*'){
+    cout<<a*b<<endl;
+    }
+    else if (c=='%')
+    cout<<a%b<<endl;
+}
