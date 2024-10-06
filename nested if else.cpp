746fca56f0cf2e3367f27c795315206ea9e073e0
devashishkumar7 <btech60047.22@bitmesra.ac.in>
2024-10-06 14:31:38 +0530

    Create nested if else.cpp

---
+++ nested if else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter the three numbers ";
+    cin>>a>>b>>c;
+if(a>b){
+  if (a>c)
+{
+    cout<<"the largest number is "<<a<<endl;
+
+}
+else 
+cout<<"the greatest number is "<<c<<endl;
+    }
+    else 
+    cout<<"the greatest number is "<<b<<endl;
+
+}
