eb1a5b6f324327e56802b429518c8310bb2fab83
devashishkumar7 <btech60047.22@bitmesra.ac.in>
2024-10-07 20:57:15 +0530

    printtable.cpp

---
+++ printtable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+ cout<<"enter the value of table of  table wanted"<<endl;
+   int n;
+    cin>>n;
+    cout<<"the table is as follows"<<endl;
+    for (int i=n;n<=100;n=n+i)
+    {
+        cout<<n<<endl;
+    }
+    
+    }
