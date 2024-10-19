3aa7cc4f09acbf124598d3a680250edaf3d793bd
devashishkumar7 <btech60047.22@bitmesra.ac.in>
2024-10-19 19:06:46 +0530

    Add files via upload

---
+++ day22.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='1';
+    int num=1;
+switch ('1'){
+case 2: cout << "hello";
+break;
+ case '1': switch(num)
+ {
+case 1: cout<<" value of num"<<endl;
+break;
+ }
+  break;  
+} 
+    
+
+
+}
